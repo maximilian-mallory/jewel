46182eddc86ddfb7d1025f24558bc0363196751b
MonnollyJeremy <monnollyj9027@my.uwstout.edu>
2024-10-20 14:42:11 -0500

    2

--- jewel/lib/notifications.dart
+++ jewel/lib/notifications.dart
@@ -5,6 +5,7 @@ import 'dart:ui';
 import 'package:awesome_notifications/awesome_notifications.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:jewel/main.dart';
 
 class NotificationController{
   static ReceivedAction? initialAction;
@@ -45,14 +46,39 @@ static Future<void> startListeningNotificationEvents() async {
 
 @pragma('vm:entry-point')
 static Future<void> onActionReceivedMethod(
-ReceivedAction ReceivedAction
+ReceivedAction receivedAction
 ) async{
-  if(ReceivedAction.actionType== ActionType.SilentAction || ReceivedAction.actionType==ActionType.SilentBackgroundAction){
+  if(receivedAction.actionType== ActionType.SilentAction || receivedAction.actionType==ActionType.SilentBackgroundAction){
+    print('Message sent via notification input: "$ReceivedAction.buttonKeyInput}"');
+    await executeLongTaskInBackground();
+  }else {
+      // this process is only necessary when you need to redirect the user
+      // to a new page or use a valid context, since parallel isolates do not
+      // have valid context, so you need redirect the execution to main isolate
+      if (receivePort == null) {
+        print(
+            'onActionReceivedMethod was called inside a parallel dart isolate.');
+        SendPort? sendPort =
+            IsolateNameServer.lookupPortByName('notification_action_port');
+
+        if (sendPort != null) {
+          print('Redirecting the execution to main isolate process.');
+          sendPort.send(receivedAction);
+          return;
+        }
+      }
+
+      return onActionReceivedImplementationMethod(receivedAction);
+    }
+}
+
+static Future<void> onActionReceivedImplementationMethod(
+  ReceivedAction receivedAction) async{
+    MyApp.navigatorKey.currentState?.pushNamedAndRemovedUntil(
+      '/notification-page', (route)=> (route.settings.name != '/notification-page')|| route.isFirst,
+     arguments: receivedAction); 
     
   }
-
-
-}
 
 
 
