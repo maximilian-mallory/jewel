1a72cfb88659bb5d410a706952396dc5b06bf25d
MonnJere <157535276+MonnJere@users.noreply.github.com>
2024-10-21 00:39:13 -0500

    initial commit

--- jewel/lib/auth/home.dart
+++ jewel/lib/auth/home.dart
@@ -1,8 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_ui_auth/firebase_ui_auth.dart';
 import 'package:flutter/material.dart';
-
-
+import 'package:jewel/google_events.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -77,6 +76,17 @@ class HomeScreen extends StatelessWidget {
               style: Theme.of(context).textTheme.displaySmall,
             ),
             const SignOutButton(),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => CalendarIntegrationExample(),
+                  ),
+                );
+              },
+              child: const Text('View Google Calendar Events'),
+            )
           ],
         ),
       ),
---
+++ jewel/lib/google_events.dart
@@ -0,0 +1,77 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:googleapis/calendar/v3.dart' as calendar;
+import 'package:http/http.dart' as http;
+import 'package:flutter/material.dart';
+
+class CalendarIntegrationExample extends StatefulWidget {
+  @override
+  _CalendarIntegrationExampleState createState() => _CalendarIntegrationExampleState();
+}
+
+class _CalendarIntegrationExampleState extends State<CalendarIntegrationExample> {
+  List<calendar.Event> _events = [];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Google Calendar Events'),
+      ),
+      body: ListView.builder(
+        itemCount: _events.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(_events[index].summary ?? 'No Title'),
+            subtitle: Text(_events[index].start?.dateTime?.toString() ?? 'No Date'),
+          );
+        },
+      ),
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _getGoogleAccessToken();
+  }
+
+  Future<void> _getGoogleAccessToken() async {
+    User? user = FirebaseAuth.instance.currentUser;
+
+    if (user != null) {
+      final googleAuth = await user.getIdTokenResult(true);
+      final accessToken = googleAuth.token;
+
+      if (accessToken != null) {
+        await _fetchEvents(accessToken);
+      }
+    } else {
+      print('User is not signed in');
+    }
+  }
+
+  Future<void> _fetchEvents(String accessToken) async {
+    var client = http.Client();
+    var authenticatedClient = AuthenticatedClient(client, accessToken);
+
+    var calendarApi = calendar.CalendarApi(authenticatedClient);
+    var events = await calendarApi.events.list("primary");
+
+    setState(() {
+      _events = events.items!;
+    });
+  }
+}
+
+class AuthenticatedClient extends http.BaseClient {
+  final http.Client _client;
+  final String _accessToken;
+
+  AuthenticatedClient(this._client, this._accessToken);
+
+  @override
+  Future<http.StreamedResponse> send(http.BaseRequest request) {
+    request.headers['Authorization'] = 'Bearer $_accessToken';
+    return _client.send(request);
+  }
+}
