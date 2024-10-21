7add08b5dd0f72d912468e052918d58a20e8f76a
MonnollyJeremy <monnollyj9027@my.uwstout.edu>
2024-10-21 15:20:30 -0500

    fixed awesome notification package

--- jewel/macos/Flutter/GeneratedPluginRegistrant.swift
+++ jewel/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,22 +5,16 @@
 import FlutterMacOS
 import Foundation
 
-<<<<<<< HEAD
 import awesome_notifications
-=======
 import cloud_firestore
->>>>>>> b7c95ac6697cafd51e35ad09bf9fa4553a02a49c
 import desktop_webview_auth
 import firebase_auth
 import firebase_core
 import google_sign_in_ios
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-<<<<<<< HEAD
   AwesomeNotificationsPlugin.register(with: registry.registrar(forPlugin: "AwesomeNotificationsPlugin"))
-=======
   FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
->>>>>>> b7c95ac6697cafd51e35ad09bf9fa4553a02a49c
   DesktopWebviewAuthPlugin.register(with: registry.registrar(forPlugin: "DesktopWebviewAuthPlugin"))
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
