8263334bfeebf315bd38d33f45c1e5fe1a789ab0
MonnollyJeremy <monnollyj9027@my.uwstout.edu>
2024-11-06 14:53:08 -0600

    isAddress updates

--- jewel/lib/google/calendar/g_g_merge.dart
+++ jewel/lib/google/calendar/g_g_merge.dart
@@ -151,8 +151,8 @@ Future<void> storeMockEvents() async {
 Future<Map<String, dynamic>> fetchEventData() async {
   try {
     DocumentSnapshot documentSnapshot = await FirebaseFirestore.instance
-        .collection('jewel_users') // Collection name
-        .doc('external') // Document name
+        .collection('data') // Collection name
+        .doc('mockEvents') // Document name
         .get();
     if (documentSnapshot.exists) {
       return googleCalendarMerge(documentSnapshot.data());
--- jewel/lib/widgets/home_screen.dart
+++ jewel/lib/widgets/home_screen.dart
@@ -3,7 +3,7 @@ import 'package:jewel/google/calendar/google_events.dart';
 import 'package:jewel/screens/test_screen1.dart';
 import 'package:jewel/screens/test_screen2.dart';
 import 'package:jewel/screens/test_screen3.dart';
-import 'package:jewel/screens/map_screen.dart';
+import 'package:jewel/google/maps/map_screen.dart';
 import 'package:jewel/widgets/calendar_event_list.dart';
 import 'package:jewel/widgets/custom_nav.dart';
 
