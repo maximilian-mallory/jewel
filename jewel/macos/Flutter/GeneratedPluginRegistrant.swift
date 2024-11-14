5b84e0e34e256633e454097c38eb2c443f8ed833
Max Mallory <mallorym4013@my.uwstout.edu>
2024-11-14 12:09:38 -0600

    merge

--- jewel/macos/Flutter/GeneratedPluginRegistrant.swift
+++ jewel/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -11,8 +11,8 @@ import desktop_webview_auth
 import firebase_auth
 import firebase_core
 import google_sign_in_ios
-import webview_flutter_wkwebview
 import url_launcher_macos
+import webview_flutter_wkwebview
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AwesomeNotificationsPlugin.register(with: registry.registrar(forPlugin: "AwesomeNotificationsPlugin"))
@@ -21,6 +21,6 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
-  FLTWebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "FLTWebViewFlutterPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
+  FLTWebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "FLTWebViewFlutterPlugin"))
 }
