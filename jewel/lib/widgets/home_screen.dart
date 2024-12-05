3f96910de450e19c9b0cf8ea6c71670b581648a7
Max Mallory <mallorym4013@my.uwstout.edu>
2024-12-05 17:35:04 -0600

    delete files only

--- jewel/lib/widgets/home_screen.dart
+++ jewel/lib/widgets/home_screen.dart
@@ -26,7 +26,7 @@ class _HomeScreenState extends State<HomeScreen> {
   void initState() {
     super.initState();
     _screens = [
-      CalendarEventList(),//calendarLogic: widget.calendarLogic),
+      Screen1(),//calendarLogic: widget.calendarLogic),
       AuthenticatedCalendar(calendarLogic: widget.calendarLogic),
       //MapScreen(), // Pass CalendarLogic if needed
       Screen1()
