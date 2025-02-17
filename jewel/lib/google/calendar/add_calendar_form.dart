c8a5db23f0d3dc1925b486b52909a0b4d3a1ac13
Yorrick Mackie <yorrickmackie@gmail.com>
2025-02-17 15:58:51 -0600

    made the calendar selection drop down working with the theme

--- jewel/lib/google/calendar/add_calendar_form.dart
+++ jewel/lib/google/calendar/add_calendar_form.dart
@@ -323,14 +323,7 @@ Widget daymonthForwardButton() {
         return Container(
   decoration: BoxDecoration(
     borderRadius: BorderRadius.circular(12.0), // Add rounded corners
-    color: Colors.white, // Set background color of the container
-    boxShadow: [
-      BoxShadow(
-        color: Colors.grey.withOpacity(0.2),
-        spreadRadius: 2,
-        blurRadius: 5,
-      ),
-    ], // Optional shadow for the dropdown
+    color: Theme.of(context).scaffoldBackgroundColor, // Set background color of the container
   ),
   child: ClipRect( // Ensures content respects the container boundaries
     child: SizedBox(
