054e17afc68d172763bb782c03f6f04329dc86eb
MonnollyJeremy <monnollyj9027@my.uwstout.edu>
2025-02-19 10:24:10 -0600

    updated google_routes

--- jewel/lib/google/maps/google_routes.dart
+++ jewel/lib/google/maps/google_routes.dart
@@ -14,13 +14,20 @@ import 'package:google_maps_routes/google_maps_routes.dart';
 Future<void> drawRouteOnMap(List<Marker> eventList) async {
   //print("TESTING: $eventList");
   MapsRoutes route = new MapsRoutes();
-  String apiKey = dotenv.env['GOOGLE_GEO_KEY']!;
+  String apiKey = dotenv.env['GOOGLE_MAPS_KEY']!;
   List<LatLng> coords = [];
 
   for (var marker in eventList) {
     coords.add(LatLng(marker.position.latitude, marker.position.longitude));
     print("Coords: ${coords}");
   }
+  /*List<LatLng> points = [
+    LatLng(44.87614689999999, -91.92364239999999),
+    LatLng(44.8763198, -91.925625),
+    LatLng(44.8761658, -91.9299928)
+  ];*/
+   //print("Points: ${points}");
+
   //String formattedCoords = coords.map((coord) => '(${coord.latitude}, ${coord.longitude})').join(', ');
   //print("Formatted Coords: $formattedCoords");
 
