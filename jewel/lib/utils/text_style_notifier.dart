42b59bd1bc26ae1ac1192d0129306b3c6690efd9
Yorrick Mackie <yorrickmackie@gmail.com>
2025-04-07 10:59:26 -0500

    changed all static colors to use the theme color

--- jewel/lib/utils/text_style_notifier.dart
+++ jewel/lib/utils/text_style_notifier.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
 import 'app_themes.dart';
 
+Color brightenColor(Color color, [double amount = 0.2]) {
+  final hsl = HSLColor.fromColor(color);
+  final hslLight = hsl.withLightness((hsl.lightness + amount).clamp(0.0, 1.0));
+  return hslLight.toColor();
+}
 class ThemeStyleNotifier extends ChangeNotifier {
   // Holds the current text style setting.
   String _textStyle = 'default';
