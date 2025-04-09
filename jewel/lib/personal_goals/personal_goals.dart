1a0984342aa0bc77636dd6d6dd8ecc5a3aa81a73
Yorrick Mackie <yorrickmackie@gmail.com>
2025-04-09 11:02:08 -0500

    add back files to get rid of errors

--- jewel/lib/personal_goals/personal_goals.dart
+++ jewel/lib/personal_goals/personal_goals.dart
@@ -1,7 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:json_annotation/json_annotation.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-
+ 
 /*
 Personal Goals Class:
 -Creates the information needed for the personal goals
@@ -10,22 +10,42 @@ Personal Goals Class:
 part 'personal_goals.g.dart';
 @JsonSerializable()
 class PersonalGoals{
-
+ 
   late String? ownerEmail; //holds the user email
   String title = ""; //title of the goal the user sets
   String description = ""; //description of the goal the user sets
   int duration = 0; //time the goal took (building for tracking purposes and analytics later)
   bool completed = false; //determines if the goal has been completed -> eventually used to determine if it should be showed in current goals or archive
   String category = ""; //Categorizes goals, will have options on goal creation form
-
+ 
   //constructor(s)
   PersonalGoals(this.title,this.description,this.category,this.completed,this.duration);
-
+ 
   factory PersonalGoals.fromJson(Map<String, dynamic> json) => _$PersonalGoalsFromJson(json);
   Map<String, dynamic> toJson() => _$PersonalGoalsToJson(this);
   //Puts Map<String, dynamic> into firebase
-  Future<void> storeGoal() async{
+  Future<void> storeGoal() async {
     ownerEmail = FirebaseAuth.instance.currentUser?.email;
-    await FirebaseFirestore.instance.collection('goals').doc(category).collection(ownerEmail!).doc().set(toJson());
+    await FirebaseFirestore.instance
+        .collection('goals')
+        .doc(category)
+        .collection(ownerEmail!)
+        .doc()
+        .set(toJson());
+  }
+ 
+  // Updates an existing goal in Firebase
+  Future<void> updateGoal(String docId) async {
+    ownerEmail = FirebaseAuth.instance.currentUser?.email;
+    if (ownerEmail == null) {
+      throw Exception("User is not logged in.");
+    }
+ 
+    await FirebaseFirestore.instance
+        .collection('goals')
+        .doc(category)
+        .collection(ownerEmail!)
+        .doc(docId)
+        .update(toJson());
   }
 }
--- jewel/macos/Flutter/GeneratedPluginRegistrant.swift
+++ jewel/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -11,8 +11,10 @@ import file_picker
 import firebase_auth
 import firebase_core
 import firebase_storage
+import flutter_local_notifications
 import google_sign_in_ios
 import location
+import shared_preferences_foundation
 import url_launcher_macos
 import webview_flutter_wkwebview
 
@@ -23,8 +25,10 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTFirebaseStoragePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseStoragePlugin"))
+  FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
   FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
   LocationPlugin.register(with: registry.registrar(forPlugin: "LocationPlugin"))
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
   WebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "WebViewFlutterPlugin"))
 }
