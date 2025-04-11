6eb6b9d4c1ef4f159eae4dd852a717a8ed6e4888
Max Mallory <mallorym4013@my.uwstout.edu>
2025-04-11 10:32:16 -0500

    modify: stash

--- jewel/macos/Flutter/GeneratedPluginRegistrant.swift
+++ jewel/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -11,8 +11,10 @@ import file_picker
 import firebase_auth
 import firebase_core
 import firebase_storage
+import flutter_secure_storage_macos
 import google_sign_in_ios
 import location
+import path_provider_foundation
 import url_launcher_macos
 import webview_flutter_wkwebview
 
@@ -23,8 +25,10 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTFirebaseStoragePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseStoragePlugin"))
+  FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
   FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
   LocationPlugin.register(with: registry.registrar(forPlugin: "LocationPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
   WebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "WebViewFlutterPlugin"))
 }
